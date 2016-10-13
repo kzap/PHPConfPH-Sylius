91247329196256c03ee84797d3840e5efbaad3d6
Andre Marcelo-Tanner <andre@enthropia.com>
2016-10-14 05:39:42 +0800

    - adding autoloader for /Lib

--- includes/application_top.php
+++ includes/application_top.php
@@ -2,6 +2,14 @@
 
 // auto load composer
 require_once __DIR__.'/../vendor/autoload.php';
+require_once __DIR__.'/autoload.php';
+
+
+$loader = new \Example\Psr4AutoloaderClass;
+// register the autoloader
+$loader->register();
+// register the base directories for the namespace prefix
+$loader->addNamespace('Lib', __DIR__.'/../Lib');
 
 // connect to database
 $mysqli = new mysqli('127.0.0.1', 'phpconfph', '', 'phpconfph_sylius');
