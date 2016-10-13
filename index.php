7d420063732405af97e3d8e7381b280cc12403fd
Andre Marcelo-Tanner <andre@enthropia.com>
2016-10-13 22:20:08 +0800

    adding bootstrap 3.3.7

--- index.php
+++ index.php
@@ -2,5 +2,7 @@
 include('includes/application_top.php');
 ?>
 <?php include('includes/header.php'); ?>
-testing 123
+<header>
+	<h1>testing 123</h1>
+</header>
 <?php include('includes/footer.php'); ?>
